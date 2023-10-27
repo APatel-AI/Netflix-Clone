8b0a19ad97bb283e16b04bcc3633b13313af470e
APatel-AI <arpan.p.se@gmail.com>
2023-10-27 07:01:24 -0500

    add: logo, shifted templates

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthContextProvider } from './context/AuthContext';
 import Home from './pages/Home';
-
-
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -12,6 +14,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
      
         </Routes>
       </AuthContextProvider>
